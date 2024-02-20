9b0ca32b71b8629f18c5878e5306bcea2312f5b6
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-02-20 04:01:06 +0000

    checkpoint

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState('');
+	const [tasksList, setTasksList] = useState([]);
+
+	const handleTask = (e) => {
+		if (e.key == "Enter") {
+			setTasksList([...tasksList,
+			{
+				label: task,
+				done: false,
+
+			}])
+		}
+
+		setTask(e.target.value);
+	}
+
+	return (<>
+		<h1 className="d-flex justify-content-center my-4">To do List {task}</h1>
+
+		<div className="caja" style={{ width: "55%", margin: "auto" }}>
+
+			<div className="" >
+				<input className="border border-0 caja  py-3 px-3 " type="text" placeholder="Task"
+					onKeyUp={(evt) => handleTask(evt)} />
+			</div>
+
+			{tasksList.map((item, index) => {
+				return <div className="caja" key={index}>
+					<div className="margen" >
+						<p className="px-4 py-3 m-0 ">{item.label}</p>
+					</div>
+				</div>
+			})}
+			<div className="border-top px-4 py-3 m-0">
+				<span className="">Item left</span>
+			</div>
 		</div>
-	);
+	</>
+
+	)
 };
 
 export default Home;
